ac6e4c512fd4b21d0eb08e8c42eb88b758c863f8
pungentee <pungentee@gmail.com>
2024-07-22 20:21:27 +0300

    updated nvim colorscheme

--- .config/nvim/lua/pungentee/plugins/colorsheme.lua
+++ .config/nvim/lua/pungentee/plugins/colorsheme.lua
@@ -14,82 +14,82 @@ local transparent_background = function()
 end
 
 return {
-	-- {
-	-- 	"f-person/auto-dark-mode.nvim",
-	--
-	-- 	dependencies = {
-	-- 		"mellow-theme/mellow.nvim",
-	-- 		-- "nyoom-engineering/oxocarbon.nvim",
-	-- 		-- "NTBBloodbath/sweetie.nvim",
-	-- 		"NLKNguyen/papercolor-theme",
-	--
-	-- 		"nvim-lualine/lualine.nvim",
-	-- 	},
-	--
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	init = function()
-	-- 		local auto_dark_mode = require("auto-dark-mode")
-	--
-	-- 		auto_dark_mode.setup({
-	-- 			update_interval = 1000,
-	-- 			set_dark_mode = function()
-	-- 				vim.o.background = "dark"
-	--
-	-- 				vim.cmd.colorscheme("mellow")
-	--
-	-- 				require("lualine").setup({
-	-- 					sections = {
-	-- 						lualine_y = {
-	-- 							{ "diff", separator = "", padding = { left = 1, right = 1 } },
-	-- 							{ "diagnostics", separator = "", padding = { left = 1, right = 1 } },
-	-- 						},
-	-- 						lualine_z = {
-	-- 							{ "location", separator = "", padding = { left = 0, right = 1 } },
-	-- 						},
-	-- 					},
-	-- 				})
-	--
-	-- 				transparent_background()
-	-- 			end,
-	--
-	-- 			-- set_dark_mode = function()
-	-- 			-- 	vim.o.background = "dark"
-	-- 			--
-	-- 			-- 	vim.cmd.colorscheme("oxocarbon")
-	-- 			--
-	-- 			-- 	transparent_background()
-	-- 			-- end,
-	--
-	-- 			set_light_mode = function()
-	-- 				vim.o.background = "light"
-	-- 				vim.g.PaperColor_Theme_Options = {
-	-- 					theme = {
-	-- 						default = {
-	-- 							transparent_background = 1,
-	-- 						},
-	-- 					},
-	-- 				}
-	--
-	-- 				vim.cmd.colorscheme("PaperColor")
-	--
-	-- 				require("lualine").setup({
-	-- 					sections = {
-	-- 						lualine_y = {
-	-- 							{ "location", separator = "", padding = { left = 0, right = 1 } },
-	-- 						},
-	-- 						lualine_z = {
-	-- 							{ "diff", separator = "", padding = { left = 1, right = 1 } },
-	-- 							{ "diagnostics", separator = "", padding = { left = 1, right = 1 } },
-	-- 						},
-	-- 					},
-	-- 				})
-	--
-	-- 				transparent_background()
-	-- 			end,
-	-- 		})
-	-- 	end,
-	-- },
+	{
+		"f-person/auto-dark-mode.nvim",
+
+		dependencies = {
+			"mellow-theme/mellow.nvim",
+			-- "nyoom-engineering/oxocarbon.nvim",
+			"NTBBloodbath/sweetie.nvim",
+			-- "NLKNguyen/papercolor-theme",
+
+			"nvim-lualine/lualine.nvim",
+		},
+
+		lazy = false,
+		priority = 1000,
+		init = function()
+			local auto_dark_mode = require("auto-dark-mode")
+
+			auto_dark_mode.setup({
+				update_interval = 1000,
+				set_dark_mode = function()
+					vim.o.background = "dark"
+
+					vim.cmd.colorscheme("mellow")
+
+					require("lualine").setup({
+						sections = {
+							lualine_y = {
+								{ "diff", separator = "", padding = { left = 1, right = 1 } },
+								{ "diagnostics", separator = "", padding = { left = 1, right = 1 } },
+							},
+							lualine_z = {
+								{ "location", separator = "", padding = { left = 0, right = 1 } },
+							},
+						},
+					})
+
+					transparent_background()
+				end,
+
+				-- set_dark_mode = function()
+				-- 	vim.o.background = "dark"
+				--
+				-- 	vim.cmd.colorscheme("oxocarbon")
+				--
+				-- 	transparent_background()
+				-- end,
+
+				set_light_mode = function()
+					vim.o.background = "light"
+					vim.g.PaperColor_Theme_Options = {
+						theme = {
+							default = {
+								transparent_background = 1,
+							},
+						},
+					}
+
+					vim.cmd.colorscheme("sweetie")
+
+					-- require("lualine").setup({
+					-- 	sections = {
+					-- 		lualine_y = {
+					-- 			{ "location", separator = "", padding = { left = 0, right = 1 } },
+					-- 		},
+					-- 		lualine_z = {
+					-- 			{ "diff", separator = "", padding = { left = 1, right = 1 } },
+					-- 			{ "diagnostics", separator = "", padding = { left = 1, right = 1 } },
+					-- 		},
+					-- 	},
+					-- })
+
+					transparent_background()
+				end,
+			})
+		end,
+	},
 
 	-- {
 	-- 	"mellow-theme/mellow.nvim",
@@ -102,17 +102,17 @@ return {
 	-- 	end,
 	-- },
 
-	{
-		"nyoom-engineering/oxocarbon.nvim",
-		lazy = false,
-		priority = 1000,
-		init = function()
-			vim.o.background = "dark"
-			vim.cmd.colorscheme("oxocarbon")
-
-			transparent_background()
-		end,
-	},
+	-- {
+	-- 	"nyoom-engineering/oxocarbon.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	init = function()
+	-- 		vim.o.background = "dark"
+	-- 		vim.cmd.colorscheme("oxocarbon")
+	--
+	-- 		transparent_background()
+	-- 	end,
+	-- },
 
 	-- {
 	-- 	"NTBBloodbath/sweetie.nvim",
